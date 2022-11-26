503f347725b845a409da3dbf3e19cd5e5fb6067c
lambui <lambui@lecle.co.kr>
2022-11-26 11:53:14 +0700

    [add] asset

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-//images used in this app
-// const backgroundBasic = 'assets/images/background/backgroundBasic.png';
-// const backgroundOnboarding = 'assets/images/background/backgroundOnboarding.png';
-// const backgroundAuthentication = 'assets/images/background/backgroundAuthentication.png';
+class ImageAssets {
+  static const String _pathPrefix = 'assets/images/png';
+
+  // START FROM HERE
+  static const String example_png = '$_pathPrefix/example_png.png';
+  static const String discountFood_png = '$_pathPrefix/im_discout_food.png';
+  static const String food_png = '$_pathPrefix/img_food.png';
+  static const String start_off_png = '$_pathPrefix/img_star_off.png';
+
+}
+
+// Contains vector image(svg)
+class VectorImageAssets {
+  static const String _pathPrefix = 'assets/images/svg';
+
+  // START FROM HERE
+  static const String example_svg = '$_pathPrefix/example_svg.svg';
+}
