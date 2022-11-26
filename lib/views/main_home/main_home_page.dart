101ea6f193d8f33d41ffab182b67e52a5c97b2f0
lambui <lambui@lecle.co.kr>
2022-11-26 13:06:59 +0700

    [update]

--- lib/views/main_home/main_home_page.dart
+++ lib/views/main_home/main_home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../constants/images.dart';
 
 class MainHomePage extends StatefulWidget {
   const MainHomePage({super.key});
@@ -9,14 +12,51 @@ class MainHomePage extends StatefulWidget {
 }
 
 class _MainHomePageState extends State<MainHomePage> {
+  final ScrollController controller = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        physics: PageScrollPhysics(),
-        slivers: [
-          
-        ]
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/png/main_home.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          CustomScrollView(
+            scrollDirection: Axis.vertical,
+            controller: controller,
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                title: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: NetworkImage(
+                          'https://i.pinimg.com/originals/da/51/c2/da51c26fe3398b0f8314fee17a98e0e7.jpg'),
+                    ),
+                    Expanded(
+                      child: Text(
+                        '1226 Univercity Road',
+                        style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                              fontSize: 16.0,
+                              height: 19.0 / 16.0,
+                            ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Icon(Iconsax.notification),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
