7d60bc4339d8ba3ec09c4cbf86376b530dae2d8a
nhatkb2001 <62232937+nhatkb2001@users.noreply.github.com>
2022-11-26 10:39:47 +0700

    done add colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+//colors used in this app
+
+const Color alt900 = Color(0xFF156800);
+const Color alt700 = Color(0xFF156800);
+const Color alt500 = Color(0xFF36A54F);
+const Color alt400 = Color(0xFF6DCE5D);
+const Color alt300 = Color(0xFF94E7A6);
+const Color alt100 = Color(0xFFDFFFDE);
+
+const Color grey900 = Color(0xFF282828);
+const Color grey700 = Color(0xFF3B3B3B);
+const Color grey500 = Color(0xFF7A7A7A);
+const Color grey200 = Color(0xFFCFCFCF);
+const Color grey100 = Color(0xFFF5F5F5);
+
+const Color white = Colors.white;
+
+const Color black = Colors.black;
+
+const Color success = Color(0xFF86B81A);
+const Color error = Color(0xFFEF3F26);
+const Color alert = Color(0xFFFFD422);
+const Color info = Color(0xFF037CEC);
+
+const Color orange = Color(0xFFF7A961);
+const Color purple = Color(0xFF9747FF);
+const Color green = Color(0xFF43C541);
